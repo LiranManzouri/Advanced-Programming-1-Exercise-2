ac993f677aad3e93274778ca46eb111ad69e03b2
LiranManzouri <liranman77@gmail.com>
2022-08-06 19:48:33 +0300

    Chatting is now working

--- Client.cpp
+++ Client.cpp
@@ -1,69 +1,53 @@
-//
-// Created by liran on 06/08/2022.
-//
-
-//#include "Client.h"
-#include "ReadFlowers.h"
-
-#include <sys/socket.h>
-#include <cstdlib>
-#include <netinet/in.h>
-#include <cstring>
-#include <unistd.h>
 #include <iostream>
-
-#define PORT 5933
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
-
-    cout << PORT << endl;
-
+int main() {
+    const char *ip_address = "127.0.0.1";
+    const int port_no = 5555;
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
-        cout << "Failed creating a socket!" << endl;
+        cout << "Error creating socket in CLIENT" << endl;
         exit(1);
     }
-
     struct sockaddr_in sin{};
     memset(&sin, 0, sizeof(sin));
-
     sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = INADDR_ANY;
-
-    sin.sin_port = htons(PORT);
-
+    sin.sin_addr.s_addr = inet_addr(ip_address);
+    sin.sin_port = htons(port_no);
     if (connect(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
-        cout << "Failed connecting!" << endl;
+        cout << "Error connecting to server in CLIENT" << endl;
         exit(1);
     }
-
-    char messages[] = "The Fu MSG";
-//  string messages[] = {argv[0], argv[1]};
-    unsigned long messages_len = /*arr[0].length() + arr[1].length()*//*2*/10;
-    long sent_bytes = send(sock, /*message*/messages, messages_len, 0);
-
-    if (sent_bytes < 0) {
-        cout << "Failed sending the info!" << endl;
-        exit(1);
+    char data_addr[] = "Im a message";
+    unsigned long data_len = strlen(data_addr);
+    int num = 1;
+    while (num != 6) {
+        num++;
+        cout << "Enter message from client to server: " << endl;
+        cin >> data_addr;
+        long sent_bytes = send(sock, data_addr, data_len, 0);
+        if (sent_bytes < 0) {
+            cout << "Error sending to server in CLIENT" << endl;
+            exit(1);
+        }
+        char buffer[4096];
+        int expected_data_len = sizeof(buffer);
+        long read_bytes = recv(sock, buffer, expected_data_len, 0);
+        if (read_bytes == 0) {
+            cout << "Closed connection in CLIENT" << endl;
+        } else if (read_bytes < 0) {
+            cout << "Error reading in Client" << endl;
+            exit(1);
+        } else {
+            cout << buffer << endl;
+        }
     }
-
-    char buffer[4096] = {0};
-    int expected_data_len = sizeof(buffer) / sizeof(char);
-
-    long read_bytes = recv(sock, buffer, expected_data_len, 0);
-    if (read_bytes == 0) {
-        cout << "connection is closed" << endl;
-        return 1;
-    }
-
-    if (read_bytes < 0) {
-        cout << "Failed receiving the info!" << endl;
-        exit(1);
-    }
-
-    cout << "Data received: " << buffer << endl;
-
     close(sock);
+    return 0;
 }
--- Server.cpp
+++ Server.cpp
@@ -1,87 +1,60 @@
-//
-// Created by liran on 06/08/2022.
-//
-
-#include "Server.h"
-#include <sys/socket.h>
-#include <iostream>
 #include <netinet/in.h>
-#include <cstring>
 #include <unistd.h>
-
-#define PORT 5933
+#include <cstring>
+#include <iostream>
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
-
-    cout << PORT << endl;
-
+int main() {
+    const int server_port = 5555;
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
-        cout << "Failed creating a socket - SERVER" << endl;
+        cout << "Error creating socket in SERVER" << endl;
         exit(1);
     }
-
     struct sockaddr_in sin{};
     memset(&sin, 0, sizeof(sin));
-
     sin.sin_family = AF_INET;
     sin.sin_addr.s_addr = INADDR_ANY;
-    sin.sin_port = htons(PORT);
-
+    sin.sin_port = htons(server_port);
     if (bind(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
-        cout << "Failed binding" << endl;
+        cout << "Error binding socket in SERVER" << endl;
         exit(1);
     }
-
-    cout << "Binding Succeed" << endl;
-
     if (listen(sock, 5) < 0) {
-        cout << "Failed listening" << endl;
+        cout << "Error listening to a socket in SERVER" << endl;
         exit(1);
     }
-
-    cout << "Listening Succeed" << endl;
-
     struct sockaddr_in client_sin{};
-    cout << "success - 1" << endl;
     unsigned int addr_len = sizeof(client_sin);
-    cout << "success - 2" << endl;
     int client_sock = accept(sock, (struct sockaddr *) &client_sin, &addr_len);
-    cout << "success - 3" << endl;
     if (client_sock < 0) {
-        cout << "Failed getting the client socket!" << endl;
+        cout << "Error accepting client in SERVER" << endl;
         exit(1);
     }
-
-    cout << "Success! Got the client socket!" << endl;
-
-    char buffer[4096];
-    int expected_data_len = sizeof(buffer);
-    long read_bytes = recv(client_sock, buffer, expected_data_len, 0);
-    if (read_bytes == 0) {
-        cout << "connection is closed" << endl;
-        return 1;
-    } else if (read_bytes < 0) {
-        cout << "Failed reading the info - SERVER" << endl;
-        exit(1);
+    int num = 1;
+    while (num != 6) {
+        char buffer[4096];
+        int expected_data_len = sizeof(buffer);
+        num++;
+        long read_bytes = recv(client_sock, buffer, expected_data_len, 0);
+        if (read_bytes == 0) {
+            cout << "Closed connection in SERVER" << endl;
+        } else if (read_bytes < 0) {
+            cout << "Error reading in SERVER" << endl;
+            exit(1);
+        } else {
+            cout << buffer << endl;
+        }
+        cout << "Enter message from server to client: " << endl;
+        cin >> buffer;
+        long sent_bytes = send(client_sock, buffer, read_bytes, 0);
+        if (sent_bytes < 0) {
+            cout << "Error sending to client in SERVER" << endl;
+            exit(1);
+        }
     }
-
-    cout << "SERVER - Data received: " << buffer << endl;
-
-    char data_addr[] = "K-N-N Algo";
-    int data_len = 10;
-
-    long send_bytes = send(sock, data_addr, data_len, 0);
-
-    if (send_bytes < 0) {
-        cout << "Failed sending the info! SERVER" << endl;
-        exit(1);
-    }
-
-    cout << "Sent successfully!" << endl;
-
-    close(client_sock);
-
+    cout << "Closing socket in SERVER" << endl;
+    close(sock);
+    return 0;
 }
