8655f0e1ec0e2ba1ca601cf91344700677d8f8f8
LiranManzouri <liranman77@gmail.com>
2022-08-14 23:33:24 +0300

    Ready for documentation.

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -6,17 +6,29 @@
 
 using namespace std;
 
+/*
+ * Main for the client, to communicate with the server.
+ * The client sends a path to unclassified flowers to the server and get their classify,
+ * after that, the client writes the classifiers to the output file.
+ */
 int main(int argc, char const *argv[]) {
+    // Responsible for the communication.
     ClientFront front;
+    // Saves the message.
     char message[4096] = "";
+    // Stops the communication after getting the close message.
     while (strcmp(message, "close") != 0) {
+        // Asks for the paths.
         cout << "Enter the unclassified path and the desired output path:" << endl;
         cin.getline(message, 4096);
+        // Stops after "close".
         if (strcmp(message, "close") == 0) {
             break;
         }
+        // Saves the paths.
         char unclassifiedPath[4096];
         char outputClassifiedPath[4096];
+        // Gets the paths.
         int i = 0, k = 0;
         while (message[i] != ' ') {
             unclassifiedPath[k] = message[i];
@@ -30,12 +42,14 @@ int main(int argc, char const *argv[]) {
             i++, k++;
         }
         outputClassifiedPath[k] = '\0';
-
+        // Send the unclassified path to the server.
         front.sendMessage(unclassifiedPath);
 
+        // Gets the types of the flowers after the server classified them.
         char *messageReceived;
         messageReceived = front.receiveMessage();
 
+        // Opens the output file.
         ofstream outputClassified;
         outputClassified.open(outputClassifiedPath);
         if (!outputClassified) {
@@ -43,8 +57,10 @@ int main(int argc, char const *argv[]) {
             exit(1);
         }
 
+        // Writes the classifiers to the output file.
         outputClassified << messageReceived << endl;
 
+        // Notifies that everything went successful.
         cout << "Output file created successfully!\n" << endl;
 
     }
