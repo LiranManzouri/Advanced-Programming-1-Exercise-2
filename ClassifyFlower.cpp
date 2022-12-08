988aaf5b2addd4f7e566f6712b5e18945cd58855
LiranManzouri <liranman77@gmail.com>
2022-12-08 14:22:39 +0200

    Idk

--- ClassifyFlower.cpp
+++ ClassifyFlower.cpp
@@ -17,41 +17,11 @@ pair<Flower *, double> *ClassifyFlower::getEuclideanDistances() const {
     return euclideanDistances;
 }
 
-pair<Flower *, double> *ClassifyFlower::getChebyshevDistances() const {
-    auto *chebyshevDistances = new pair<Flower *, double>[numOfFlowers];
-    for (int i = 0; i < numOfFlowers; i++) {
-        chebyshevDistances[i] = (pair<Flower *, double>(&flowers[i],
-                                                        DistanceCalculator::chebyshev(unclassifiedFlower, flowers[i])));
-    }
-    return chebyshevDistances;
-}
-
-pair<Flower *, double> *ClassifyFlower::getManhattanDistances() const {
-    auto *manhattanDistances = new pair<Flower *, double>[numOfFlowers];
-    for (int i = 0; i < numOfFlowers; i++) {
-        manhattanDistances[i] = (pair<Flower *, double>(&flowers[i],
-                                                        DistanceCalculator::manhattan(unclassifiedFlower, flowers[i])));
-    }
-    return manhattanDistances;
-}
-
 string ClassifyFlower::euclideanClassify() {
     pair<Flower *, double> *euclideanDistances = getEuclideanDistances();
     KNN::QuickSelect(k, euclideanDistances, 0, numOfFlowers - 1);
     return classifyByKNN(euclideanDistances);
 
-}
-
-string ClassifyFlower::chebyshevClassify() {
-    pair<Flower *, double> *chebyshevDistances = getChebyshevDistances();
-    KNN::QuickSelect(k, chebyshevDistances, 0, numOfFlowers - 1);
-    return classifyByKNN(chebyshevDistances);
-}
-
-string ClassifyFlower::manhattanClassify() {
-    pair<Flower *, double> *manhattanDistances = getManhattanDistances();
-    KNN::QuickSelect(k, manhattanDistances, 0, numOfFlowers - 1);
-    return classifyByKNN(manhattanDistances);
 }
 
 // runs the KNN algo on the array of pairs, then looks at the k first
@@ -60,14 +30,15 @@ string ClassifyFlower::classifyByKNN(pair<Flower *, double> *distances) const {
     int counterForIrisSetosa = 0;
     int counterForIrisVersicolor = 0;
     int counterForIrisVirginica = 0;
+    
     for (int i = 0; i < k; i++) {
-        if (distances[i].first->getFlowerType().compare("Iris-setosa") == 0) {
+        if (distances[i].first->getFlowerType() == "Iris-setosa") {
             counterForIrisSetosa++;
         }
-        if (distances[i].first->getFlowerType().compare("Iris-versicolor") == 0) {
+        if (distances[i].first->getFlowerType() == "Iris-versicolor") {
             counterForIrisVersicolor++;
         }
-        if (distances[i].first->getFlowerType().compare("Iris-virginica") == 0) {
+        if (distances[i].first->getFlowerType() == "Iris-virginica") {
             counterForIrisVirginica++;
         }
     }
